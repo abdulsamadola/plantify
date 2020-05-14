15fdd0bc3fd2a6a246b6e72e50934874064bf460
abdulsamadola <suleimanolamilekan03@gmail.com>
2020-05-14 09:30:25 +0100

    :sparkles: Complete Explore Screen

--- app/src/main/java/com/campstore/plantify/ui/Explore.java
+++ app/src/main/java/com/campstore/plantify/ui/Explore.java
@@ -2,7 +2,6 @@ package com.campstore.plantify.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,27 +9,21 @@ import com.campstore.plantify.R;
 
 import spencerstudios.com.bungeelib.Bungee;
 
-public class Detail extends AppCompatActivity {
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        Bungee.slideUp(this);
-    }
+public class Explore extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_explore);
+    }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Bungee.spin(this);
     }
 
     public void goBack(View view) {
         onBackPressed();
-    }
-
-    public void goToExplore(View view) {
-        startActivity(new Intent(this, Explore.class));
-        Bungee.card(this);
     }
 }
